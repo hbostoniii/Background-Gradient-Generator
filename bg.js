e1231f74a556c5e86eca770f413917cc14383989
hbostoniii <60755766+hbostoniii@users.noreply.github.com>
2020-06-22 20:23:48 -0500

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,12 @@
+let css = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let body = document.getElementById('gradient');
+
+const gradientUpdate = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    css.textContent = `${body.style.background};`
+}
+
+color1.addEventListener('input', gradientUpdate)
+color2.addEventListener('input', gradientUpdate)
